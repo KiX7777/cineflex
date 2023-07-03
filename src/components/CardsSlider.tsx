ba80f27ab457b881ea9c0456d675547abb6f3803
KiX7777 <kristianboi202@gmail.com>
2023-07-03 22:08:11 +0200

    svg animation, overflowing movie page

---
+++ src/components/CardsSlider.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import classes from './CardsSlider.module.css';
+import MovieCard from './MovieCard';
+
+const CardsSlider = () => {
+  return (
+    <div className={classes.container}>
+      <MovieCard
+        rating={4}
+        id={20}
+        title='Rocky'
+        poster='/uROT3bgu8I50EyDA541aozJjYl3.jpg'
+        overview='His teachers, coaches, childhood friends and Barça teammates, together with journalists, writers and prominent figures from the history of football, come together in a restaurant to analyze and pick apart Messis personality both on and off the field, and to look back at some of the most significant moments in his life. Viewed from Álex de la Iglesias unique perspective, Messi recreates the players childhood and teenage years, from his very first steps, with a football always at his feet, through to the decision to leave Rosario for Barcelona, the separation from his family, and the role played in his career by individuals such as Ronaldinho, Rijkaard, Rexach and Guardiola.'
+      />
+      <MovieCard
+        rating={4}
+        id={20}
+        title='Rocky'
+        poster='/uROT3bgu8I50EyDA541aozJjYl3.jpg'
+        overview='His teachers, coaches, childhood friends and Barça teammates, together with journalists, writers and prominent figures from the history of football, come together in a restaurant to analyze and pick apart Messis personality both on and off the field, and to look back at some of the most significant moments in his life. Viewed from Álex de la Iglesias unique perspective, Messi recreates the players childhood and teenage years, from his very first steps, with a football always at his feet, through to the decision to leave Rosario for Barcelona, the separation from his family, and the role played in his career by individuals such as Ronaldinho, Rijkaard, Rexach and Guardiola.'
+      />
+      <MovieCard
+        rating={4}
+        id={20}
+        title='Rocky'
+        poster='/uROT3bgu8I50EyDA541aozJjYl3.jpg'
+        overview='His teachers, coaches, childhood friends and Barça teammates, together with journalists, writers and prominent figures from the history of football, come together in a restaurant to analyze and pick apart Messis personality both on and off the field, and to look back at some of the most significant moments in his life. Viewed from Álex de la Iglesias unique perspective, Messi recreates the players childhood and teenage years, from his very first steps, with a football always at his feet, through to the decision to leave Rosario for Barcelona, the separation from his family, and the role played in his career by individuals such as Ronaldinho, Rijkaard, Rexach and Guardiola.'
+      />
+      <MovieCard
+        rating={4}
+        id={20}
+        title='Rocky'
+        poster='/uROT3bgu8I50EyDA541aozJjYl3.jpg'
+        overview='His teachers, coaches, childhood friends and Barça teammates, together with journalists, writers and prominent figures from the history of football, come together in a restaurant to analyze and pick apart Messis personality both on and off the field, and to look back at some of the most significant moments in his life. Viewed from Álex de la Iglesias unique perspective, Messi recreates the players childhood and teenage years, from his very first steps, with a football always at his feet, through to the decision to leave Rosario for Barcelona, the separation from his family, and the role played in his career by individuals such as Ronaldinho, Rijkaard, Rexach and Guardiola.'
+      />
+      <MovieCard
+        rating={4}
+        id={20}
+        title='Rocky'
+        poster='/uROT3bgu8I50EyDA541aozJjYl3.jpg'
+        overview='His teachers, coaches, childhood friends and Barça teammates, together with journalists, writers and prominent figures from the history of football, come together in a restaurant to analyze and pick apart Messis personality both on and off the field, and to look back at some of the most significant moments in his life. Viewed from Álex de la Iglesias unique perspective, Messi recreates the players childhood and teenage years, from his very first steps, with a football always at his feet, through to the decision to leave Rosario for Barcelona, the separation from his family, and the role played in his career by individuals such as Ronaldinho, Rijkaard, Rexach and Guardiola.'
+      />
+      <MovieCard
+        rating={4}
+        id={20}
+        title='Rocky'
+        poster='/uROT3bgu8I50EyDA541aozJjYl3.jpg'
+        overview='His teachers, coaches, childhood friends and Barça teammates, together with journalists, writers and prominent figures from the history of football, come together in a restaurant to analyze and pick apart Messis personality both on and off the field, and to look back at some of the most significant moments in his life. Viewed from Álex de la Iglesias unique perspective, Messi recreates the players childhood and teenage years, from his very first steps, with a football always at his feet, through to the decision to leave Rosario for Barcelona, the separation from his family, and the role played in his career by individuals such as Ronaldinho, Rijkaard, Rexach and Guardiola.'
+      />
+    </div>
+  );
+};
+
+export default CardsSlider;
